ec00a2544823851d049e59cf04bef3a64eab7a63
charles-dubos <charles.dubos@gmail.com>
2022-11-22 08:00:56 +0100

    Adding HOTP works

---
+++ test.py
@@ -0,0 +1,9 @@
+# $ sudo apt install python3-venv
+# $ python3 -m venv /path/to/venv
+# $ source /path/to/venv/bin/activate
+# $ pip3 install ipykernel
+# $ python3 -m ipykernel install --user --name=venv
+# $ jupyter kernelspec list
+# $ pip3 install aiosmtpd
+
+
